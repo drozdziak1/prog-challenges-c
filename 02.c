7b408f19803776ffd025920709e6002de8588c14
Stanisław Drozd <drozdziak1@gmail.com>
2016-02-20 13:41:49 +0100

    start working on 02

---
+++ 02.c
@@ -0,0 +1,11 @@
+/*
+ * 02 - Temperature converter
+ *
+ * Author: Stanisław Drozd <drozdziak1@gmail.com>
+ */
+
+int main(int argc, char const* argv[])
+{
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 CFLAGS=-Wall -g -std=c99 -lbsd
 
-objs = 01
+objs = 01 02
 
 all: $(objs)
 
